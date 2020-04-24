f4c42606b0a1b88938c70023687ee43fbf874ea8
Hoyeon Kim <kim.hoyeon93@gmail.com>
2020-04-24 01:50:22 -0700

    updated funtion formats, and remove init function

--- greeting.js
+++ greeting.js
@@ -1,41 +1,43 @@
+//1번째로 생성, 5번째동시
 const form = document.querySelector(".js-form");
 const input = form.querySelector("input");
 const greeting = document.querySelector(".js-greetings");
-
+//4번째 6번째
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
 
-const saveName = (text) => {
+//9번째
+function saveName(text) {
   localStorage.setItem(USER_LS, text);
-};
-
-const handleSubmit = (event) => {
+}
+//8번째
+function handleSubmit(event) {
   event.preventDefault();
-  const currentValue = input.value;
+  let currentValue = input.value;
   paintGreeting(currentValue);
   saveName(currentValue);
-};
-
-const askforName = () => {
+}
+//7번째
+function askforName() {
   form.classList.add(SHOWING_CN);
   form.addEventListener("submit", handleSubmit);
-};
-
-const paintGreeting = (text) => {
+}
+//5번째
+function paintGreeting(text) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
   greeting.innerText = `Hello ${text}`;
-};
-const loadName = () => {
+}
+//3번째
+function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
   if (currentUser === null) {
     askforName();
   } else {
     paintGreeting(currentUser);
   }
-};
+}
 
-const init = () => {
-  loadName();
-};
-init();
+//2번째로생성
+
+loadName();
