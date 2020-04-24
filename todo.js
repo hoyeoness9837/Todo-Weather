1894df248f95d158ee60e136421ff48ca6826b3c
Hoyeon Kim <kim.hoyeon93@gmail.com>
2020-04-24 03:36:30 -0700

    updated with 3-7 lecture contents

--- todo.js
+++ todo.js
@@ -4,7 +4,19 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
-const toDos = [];
+let toDos = [];
+
+/*아래함수는 투두리스트에서 각 텍스트의  태그의 parent 를 parentNode로 가져와서 removeChild로 없애는 기능을 추가함 그리고 filter()은 array에서 ()함수를 통과해서 조건에 맞는 아이템으로 다시 새로운 array를 만들어준다. */
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function (toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
 
 function saveToDos() {
   localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -17,6 +29,7 @@ function paintToDo(text) {
   const newId = toDos.length + 1;
   delBtn.innerHTML = "&#x274C;";
   span.innerText = text;
+  delBtn.addEventListener("click", deleteToDo);
   li.appendChild(span);
   li.appendChild(delBtn);
   li.id = newId;
@@ -35,7 +48,7 @@ function handleSubmit(event) {
   paintToDo(currentValue);
   toDoInput.value = "";
 }
-
+/*아래 함수에서 forEach로 array안에있는 각각의 엘리멘트에 function 을 사용해준다 */
 function loadToDos() {
   const loadedToDos = localStorage.getItem(TODOS_LS);
   if (loadedToDos !== null) {
