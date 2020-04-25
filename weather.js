b0fe9afe867f00c6234ea1d7384501897362b298
Hoyeon Kim <kim.hoyeon93@gmail.com>
2020-04-25 00:13:30 -0700

    updated with 3-10 lecture material

--- weather.js
+++ weather.js
@@ -1,5 +1,20 @@
+const weather = document.querySelector(".js-weather");
 const API_KEY = "90b2f773fae3862aaf2f11a1fe2f96b7";
 const COORDS = "coords";
+
+function getWeather(lat, lng) {
+  fetch(
+    `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=imperial`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temperature}@ ${place}`;
+    });
+}
 
 function saveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -13,6 +28,7 @@ function handleGeoSuccess(position) {
     longitude,
   };
   saveCoords(coordsObj);
+  getWeather(lat, lng);
 }
 
 function handleGeoError() {
@@ -28,6 +44,8 @@ function loadCoords() {
   if (loadedCoords === null) {
     askForCoords();
   } else {
+    const parseCoords = JSON.parse(loadedCoords);
+    getWeather(parseCoords.latitude, parseCoords.longitude);
   }
 }
 
