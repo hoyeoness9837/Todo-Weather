1eeeb74bf48cf41ecc4d455e49830ece98c7d778
Hoyeon Kim <kim.hoyeon93@gmail.com>
2020-04-24 19:19:26 -0700

    uploaded background JS and Img files.

--- clock.js
+++ clock.js
@@ -1,20 +1,19 @@
 const clockContainer = document.querySelector(".js-clock");
 const clockTitle = clockContainer.querySelector("h1");
 
+//getTime이라는 함수를 실행하면, Date라는 함수를 
 const getTime = () => {
   const date = new Date();
   const minutes = date.getMinutes();
   const hours = date.getHours();
   const seconds = date.getSeconds();
-  //   want to put 0 when the second is less than 10
+  //   want to put 0 when the second is less than 10, 그래서 10보다 숫자가 작을때 앞자리에 0을 표시한다.
   clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
     minutes < 10 ? `0${minutes}` : minutes
   }:${seconds < 10 ? `0${seconds}` : seconds}`;
 };
 
-// function sayhi(){console.log("hi")};
-// setInterval(sayhi,3000); <-- every 3sec, execute sayhi function
-
+// getTime이라는 함수를 1초에 한번씩 실행한다
 const init = () => {
   getTime();
   setInterval(getTime, 1000);
